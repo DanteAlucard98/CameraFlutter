e8a4393a53087a34dd41c337112d349c2f613252
Daniel98 <aperez@4awish.com>
2022-05-02 20:40:18 -0500

    camera

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:camera/camera.dart';
+import 'package:camera_video/screens/camera_Screen.dart';
+import 'package:flutter/material.dart';
+
+List<CameraDescription> cameras = [];
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error in fetching the cameras: $e');
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(),
+    );
+  }
+}
